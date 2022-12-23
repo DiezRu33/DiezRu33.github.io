0661190a3f852bb3110f3c99be95d5e2a4e7f7bb
Mr.Person <diegoruffa@hotmail.com>
2022-12-23 20:55:44 -0300

    Change this

--- script.js
+++ script.js
@@ -29,3 +29,60 @@ boton2.addEventListener("click", (b) => {
 boton3.addEventListener("click", (c) => {
     msj_mdn(boton3)
 })
+
+
+// let x = parrafo;
+// let x2 = _parrafo;
+
+// function cambioColor(){
+//     x.style.color = "#fff";
+//     x.style.transition = ".5s all";
+//     x2.style.color = "#fff";
+//     x2.style.transition = ".5s all";
+//     x.removeEventListener("mouseover", e);
+//     x2.removeEventListener("mouseover", e);
+//     x2.addEventListener("mouseout", (a) => {
+//         x.style.color = "#000";
+//     })
+//     }
+
+// class cambio {
+//     parrafo.addEventListener("mouseover", (e) => {
+//         parrafo.style.color = "#fff"
+//         parrafo.style.transition = ".5s all"
+//         parrafo.removeEventListener("mouseover", e);
+//             parrafo.addEventListener("mouseout", (a) => {
+//                 parrafo.style.color = "#000"
+//             })
+        
+//     })
+// }
+
+// function white(){
+//     parrafo.style.color = "#fff"
+//     parrafo.style.transition = ".5s all"
+// }
+
+// parrafo.addEventListener("click", (d) => {
+    //    white(parrafo)
+// })
+
+
+// function black(){
+    // parrafo.style.color = "#000"
+// }
+
+// _parrafo.addEventListener("mouseout", (e) => {
+    //    black(_parrafo2)
+// })
+
+
+// parrafo.addEventListener("click", (f) => {
+    
+//     parrafo.removeEventListener("mouseover", e);
+//         parrafo.addEventListener("mouseout", (i) => {
+            
+//         })
+    
+// })
+
