bc7fea3eac4eb15e4b2dc702004d2a8ac6c2b2db
MrPerson <diegoruffa@hotmail.com>
2022-12-23 02:32:10 -0300

    Changes in Cabecera files

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.getQuerySelector("container");
+const parrafo = document.getQuerySelector("p");
+const post = document.getQuerySelector("post");
+
+parrafo.addEventListener("click", () => {
+    alert("Hola");
+})
+
